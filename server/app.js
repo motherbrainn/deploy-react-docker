90ce342e3e10e50a87d197cc9b6c7a45e60320f6
FSpanthor <37132756+FSpanthor@users.noreply.github.com>
2022-09-06 10:48:26 -0700

    add path to app.js

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = process.env?.PORT ?? 4000;
@@ -6,7 +7,7 @@ const env = process.env.NODE_ENV;
 
 // send static client files here when in prod
 env === "production" &&
-  app.use(express.static(path.resolve(__dirname, "../client/public")));
+  app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 app.get("/example-endpoint", (req, res) => {
   res.send("Hello World!");
